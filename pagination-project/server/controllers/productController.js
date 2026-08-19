910dc0d90fefb930ad9998c617463a794e49f22b
Nikitarathod2001 <nikitarathod3601@gmail.com>
2026-08-19 20:02:58 +0530

    Implement backend pagination

--- pagination-project/server/controllers/productController.js
+++ pagination-project/server/controllers/productController.js
@@ -3,11 +3,31 @@ import Product from "../models/Product.js";
 export const getProducts = async (req, res) => {
   try {
 
-    const products = await Product.find();
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 4;
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find()
+                      .skip(skip)
+                      .limit(limit);
+
+    const totalItems = await Product.countDocuments();
+    const totalPages = Math.ceil(totalItems / limit);
+
+    const hasNextPage = page < totalPages;
+    const hasPrevPage = page > 1;
 
     res.status(200).json({
       success: true,
       products,
+      pagination: {
+        currentPage: page,
+        limit,
+        totalItems,
+        totalPages,
+        hasNextPage,
+        hasPrevPage,
+      },
     });
     
   } catch (error) {
