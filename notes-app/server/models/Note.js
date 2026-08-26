48c23b6a8ad34ee7a11bdd8d43a2473684973bd0
Nikitarathod2001 <nikitarathod3601@gmail.com>
2026-08-26 10:28:37 +0530

    Create note model

---
+++ notes-app/server/models/Note.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 100,
+  },
+
+  content: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  category: {
+    type: String,
+    enum: [
+      "Personal",
+      "Work",
+      "Study",
+      "Ideas",
+      "Projects",
+      "Todo",
+      "Important",
+      "Other",
+    ],
+    default: "Other",
+  },
+
+  isPinned: {
+    type: Boolean,
+    default: false,
+  },
+}, {timestamps: true});
+
+const Note = mongoose.models.Note || mongoose.model("Note", noteSchema);
+
+export default Note;
