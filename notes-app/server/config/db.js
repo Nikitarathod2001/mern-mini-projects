dd17d39dd77f655c605044e183b15d9ce982701e
Nikitarathod2001 <nikitarathod3601@gmail.com>
2026-08-26 10:09:18 +0530

    Database connected

---
+++ notes-app/server/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async () => {
+  try {
+
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("Database Connected!");
+    
+  } catch (error) {
+    console.log("Database connection failed");
+  }
+};
+
+export default connectDB;
--- notes-app/server/server.js
+++ notes-app/server/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+await connectDB();
 
 app.use(cors());
 app.use(express.json());
