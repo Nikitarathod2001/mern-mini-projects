1467ccb211dc90f8a39ea1c5804a5f5b0ca016c5
Nikitarathod2001 <nikitarathod3601@gmail.com>
2026-08-26 11:52:03 +0530

    Setup React frontend

--- notes-app/client/src/App.jsx
+++ notes-app/client/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      NOTES APP
-    </div>
+    <Routes>
+      <Route path='/' element={<h1 className='text-3xl font-bold'>Notes App</h1>}/>
+    </Routes>
   )
 }
 
