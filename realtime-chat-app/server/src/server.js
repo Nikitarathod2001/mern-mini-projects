2d5c8c068693498927dd9af14ba28809219c9521
Nikitarathod2001 <nikitarathod3601@gmail.com>
2026-09-25 14:49:42 +0530

    Setup backend and mongodb connection

--- realtime-chat-app/server/src/server.js
+++ realtime-chat-app/server/src/server.js
@@ -1,9 +1,22 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
 
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.json({
@@ -11,6 +24,19 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+    
+  } catch (error) {
+    console.error("Failed to start server: ", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
